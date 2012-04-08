241d94db456b7c0f56831b4382702b58c978db58
Brandon Catcho <lochnessy@gmail.com>
2012-04-08 00:33:33 -0400

    added a graphing library and some look n feel

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var request = require("request");
+
+var app = express.createServer();
+app.use(express.bodyParser());
+app.use(express.static('pub'));
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+   res.render('index');
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port);
